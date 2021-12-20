f701a9fb7af6be6817f9fae2ae30f4e701d5e41d
Rodes Chen <rodes.chen@gmail.com>
2021-12-20 18:04:45 +0800

    fix tradef not include

--- src/spot.js
+++ src/spot.js
@@ -2,13 +2,13 @@ const APIBase = require('./APIBase')
 const {
   Blvt, Bswap, SubAccount, Market, Trade, Futures, Fiat,
   Wallet, Margin, Mining, Savings, Stream, Websocket, C2C, Loan, Pay, Convert, Rebate, NFT,
-  Streamf, Websocketf,
+  Tradef, Streamf, Websocketf,
 } = require('./modules')
 const { flowRight } = require('./helpers/utils')
 
 class Spot extends flowRight(Blvt, Bswap, SubAccount, Websocket, Stream,
   Savings, Margin, Mining, Wallet, Market, Trade, Futures, Fiat, C2C, Loan, Pay, Convert, Rebate, NFT,
-  Streamf, Websocketf)(APIBase) {
+  Tradef, Streamf, Websocketf)(APIBase) {
   constructor (apiKey = '', apiSecret = '', options = {}) {
     options.baseURL = options.baseURL || 'https://api.binance.com'
     super({
